ad34463883aa4591f113cb53a87c89f608fc6785
PedTorSes <pedtorses@alu.edu.gva.es>
2025-12-18 16:39:04 +0100

    Creación de la clase Mochilero

---
+++ src/main/java/com/ieschabas/Mochilero.java
@@ -0,0 +1,21 @@
+package com.ieschabas;
+
+/**
+ * Clase Mochilero en la cual vamos a establecer un array y luego vamos a mostar la longitud del array
+ * @author Pedro Torres Sesé
+ * @version 1.0
+ */
+public class Mochilero {
+    public static void main() {
+        /**
+         * Creamos array y establecemos sus valores
+         */
+        int[] puntosDano = {15, 8, 22, 11, 30};
+        /**
+         * Mostramos la longitud del array
+         */
+        System.out.println(puntosDano.length);
+
+    }
+
+}
