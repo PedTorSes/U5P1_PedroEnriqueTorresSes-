e536bc870723cce505fb814f5014b5e77e752275
PedTorSes <pedtorses@alu.edu.gva.es>
2025-12-18 17:21:09 +0100

    Creación de la clase NotasModulo

---
+++ src/main/java/com/ieschabas/NotasModulo.java
@@ -0,0 +1,49 @@
+package com.ieschabas;
+
+import java.util.Arrays;
+
+/**
+ * Clase NotasModulo en la cual vamos a establecer un método que reciba un array de las notas y nos devuelva la media de las notas
+ * @author Pedro Torres Sesé
+ * @version 1.0
+ */
+public class NotasModulo {
+    /**
+     * Array con las notas de los alumnos
+     * @param args
+     */
+    public static void main(String[] args) {
+        double[] notas = {5, 10, 4, 3, 9, 7};
+        /**
+         * Mostramos por pantalla las notas de los alumnos
+         */
+        System.out.println("Las notas son: " + Arrays.toString(notas));
+        calcularMedia(notas);
+    }
+
+    /**
+     * Método para calcular la nota media de la clase
+     * @param notas
+     */
+
+    static void calcularMedia(double[] notas) {
+        /**
+         * Establecemos las variables para calcular la nota total y luego la media
+         */
+        double notaTotal = 0;
+        double media = 0;
+        for (int i = 0; i < notas.length; i++) {
+            /**
+             * Recorremos el array para realizar la suma de la nota total y luego dividirla entre el tamaño del array
+             */
+            notaTotal = notaTotal + notas[i];
+            media = notaTotal / notas.length;
+        }
+        /**
+         * Mostramos por pantalla los resultados de la nota media
+         */
+        System.out.println("La nota media es: " + media);
+    }
+}
+
+
