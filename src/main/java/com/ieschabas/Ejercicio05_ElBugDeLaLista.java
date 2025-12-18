0fc36d2662bf0b9ee0ffb162a80b6a24edbffa76
PedTorSes <pedtorses@alu.edu.gva.es>
2025-12-18 21:12:12 +0100

    Clase ElBugDeLaLista (faltan comentarios)

--- src/main/java/com/ieschabas/Ejercicio05_ElBugDeLaLista.java
+++ src/main/java/com/ieschabas/Ejercicio05_ElBugDeLaLista.java
@@ -1,5 +1,46 @@
 package com.ieschabas;
+/**
+ * Clase ElBugDeLaLista en la cual vamos a eliminar, de una lista de labores, la que se encuentre en la segunda posición.
+ * @author Pedro Enrique Torres Sesé
+ * @version 1.0
+ */
+
+import java.util.Arrays;
 
 public class Ejercicio05_ElBugDeLaLista {
+    public static void main(String[] args) {
 
+        /**
+         * Creamos el primer array con todas las labores a realizar
+         */
+        String[] tareasPendientes = {"Dormir", "Estudiar", "Beber", "Comer", "Jugar"};
+        /**
+         * Establecemos la posición del dato que queremos eliminar
+         */
+        int exclusion = 1;
+        /**
+         * Mostramos el array original para que se vea que no hay trampa ni cartón
+         */
+
+        System.out.println("Array original: " + Arrays.toString(tareasPendientes));
+        /**
+         * Creamos el array nuevo el cual tiene un espacio menos que el original
+         */
+        String[] tareasPendientesResultado = new String[tareasPendientes.length - 1];
+        /**
+         * Establecemos el valor de la posición que va a ocupar el nuevo elemento que introduzcamos en el array
+         */
+        int j = 0;
+        /**
+         *
+         */
+
+        for (int i = 0; i < tareasPendientes.length; i++) {
+            if (i != exclusion) {
+                tareasPendientesResultado[j] = tareasPendientes[i];
+                j++;
+            }
+        }
+        System.out.println("Array resultante: "+Arrays.toString(tareasPendientesResultado));
+    }
 }
